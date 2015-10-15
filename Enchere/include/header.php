59b26e2cf1925f928bc7b940fb2fc24069ef1c83
Milihhard <milihhard1996@gmail.com>
2015-10-15 08:56:12 +0200

    pouet

---
+++ Enchere/include/header.php
@@ -0,0 +1,13 @@
+<div id="header">
+	<div id ="banniere">
+		<img width="500" height="250"src='res/head2.jpg'>
+	</div>
+	<nav id ="navigation">
+		<ul id = "menu">
+			<li> <a href ="#">Acceuil</a> </li>
+			<li> <a href ="#">Objet</a> </li>
+			<li> <a href ="#">Nouvelle Enchere</a> </li>
+			<li> <a href ="#">Contact</a> </li>
+		</ul>				
+	</nav>	
+</div>
---
+++ Enchere/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8"/>		
+		<title>BrBa Store</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div id="main">
+			<?php  include("include/header.php")?>
+			
+		</div>
+	</body>
+	
+</html>
