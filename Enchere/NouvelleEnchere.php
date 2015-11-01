b914959ee8d76d0298a21611704f6ede031094d4
RayanMehdi <rayanmehdi1@gmail.com>
2015-11-01 23:02:01 +0100

    son pere le chat

--- Enchere/NouvelleEnchere.php
+++ Enchere/NouvelleEnchere.php
@@ -9,44 +9,7 @@ if (!$id){
 
 <!DOCTYPE html>
 <html lang="fr">
-<<<<<<< HEAD
-	<head>
-		<meta charset="utf-8"/>		
-		<title>BrBa Store</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-	</head>
-	<body>
-		<div id="main">
-			<?php include("include/header.php"); ?>
-			
-		</div>
-		<br><br><br>
-		
-		<br>
-		<div id="formulaire">
-			<h1>Nouvelle Enchere</h1>
-			<form method='POST' action='INSERTBD.php'>
-				<div id='perso'>
-					Nom<br><input type='text' name='nom' value=<?php isset($_POST['nom'])?>><br>
-					Prenom<br><input type='text' name='prenom' value=<?php isset($_POST['prenom']) ?> ><br>
-					Addresse Mail<br><input type='text' name='email' value=<?php isset($_POST['email'])?> ><br>
-					Telephone<br><input type="text" name='tel' value=<?php isset($_POST['tel'])?>><br>
-				</div>
-				<div id='obj'>
-					Nom de L'Objet<br><input type='text' name='nomObj' value=<?php isset($_POST['nomObj'])?>><br>
-					Url de L'Image<br><input type='url' name='Image'  value=<?php isset($_POST['Image'])?>><br>
-					Prix Min<br><input type='text' name='PrixMin' value=<?php isset($_POST['PrixMin'])?>><br>
-					Datedebut<br><input type='date' name='DateDebut' value=<?php isset($_POST['DateDebut'])?>><br>
-					DateFin<br><input type='date' name='DateFin' value=<?php isset($_POST['DateFin'])?>><br>
-				</div>
-					<div id='bouton'><input type="submit" name="valider" value="Valider"></div>
-			</form>
-			
-			
-		</div>
-	</body>
-	
-=======
+
 <head>
 	<meta charset="utf-8"/>
 	<title>BrBa Store</title>
