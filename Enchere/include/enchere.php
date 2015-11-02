a7572863ff24a25e2e8a458805db3f1e3316f3dd
Milihhard <milihhard1996@gmail.com>
2015-11-02 02:03:01 +0100

    sdfghj,knb

--- Enchere/include/enchere.php
+++ Enchere/include/enchere.php
@@ -7,7 +7,11 @@
 		if(isset($_SESSION["maildiff"]) and $_SESSION["maildiff"]==1){
 			echo "<p id='red'>E-mails differents</p>";
 			$_SESSION["maildiff"]=0;
-		} ?>
+		}
+		if(isset($_SESSION["tropEnchere"]) and $_SESSION["tropEnchere"]==1){
+			echo "<p id='red'>Vous avez fait trop d'enchere!</p>";
+			$_SESSION["nbEnchere"]=0;
+		}?>
 		<p id="font2">Votre prenom :</p><input type="text" name="nickname" required autofocus>
 		<p id="font2">Votre nom :</p><input type="text" name="lastname" required>
 		<p id="font2">Votre e-mail :</p><input type="text" name="mail[]" required>
--- Enchere/traite_enchere.php
+++ Enchere/traite_enchere.php
@@ -9,27 +9,56 @@
 <body>
 <?php
 include("params.inc.php");
-if($_POST['mail'][0]==$_POST['mail'][1]) {
-	$id = mysqli_connect($host, $user, $password, $dbname);
-	$requete = "SELECT * FROM utilisateur WHERE addresse like '" . $_POST['mail'][0] . "'";
-	$marche = mysqli_query($id, $requete);
-	if ($ligne = $marche->fetch_array(MYSQLI_NUM)) {
-		//bon e-mail
-		if ($ligne[1] = $_POST["lastname"] and $ligne[0] = $_POST["nickname"] and $ligne[3] = $_POST["phone"]) {
-			//bonne co
-			echo "<a href=\"index.php\">revenir sur la page acceuil</a>";
-			if (isset($_SESSION["prixObjet"]) and $_SESSION["prixObjet"] < $_POST["enchere"]) {
-				$sql = "UPDATE Objet SET Uti_idUtilisateur= ".$ligne[4].", prixEnchere=".$_POST["enchere"]." WHERE nomObjet like '".$_SESSION["nomObjet"]."'";
-				if ($id->query($sql) === TRUE) {
-					echo "<p id=\"font\">Record updated successfully</p>";
+if(isset($_SESSION["nbEnchere"]) && ($_SESSION["nbEnchere"]>4)){
+	echo "trop enchere";
+	if (isset($_SESSION["nomObjet"])) {
+		$file = "Location:enchere.php?nom=" . $_SESSION["nomObjet"];
+		$_SESSION["tropEnchere"] = 1;
+		header($file);
+	} else {
+		header('Location:objet.php');
+	}
+	$id->close();
+	exit();
+}else {
+	if ($_POST['mail'][0] == $_POST['mail'][1]) {
+		$id = mysqli_connect($host, $user, $password, $dbname);
+		$requete = "SELECT * FROM utilisateur WHERE addresse like '" . $_POST['mail'][0] . "'";
+		$marche = mysqli_query($id, $requete);
+		if ($ligne = $marche->fetch_array(MYSQLI_NUM)) {
+			//bon e-mail
+			if ($ligne[1] = $_POST["lastname"] and $ligne[0] = $_POST["nickname"] and $ligne[3] = $_POST["phone"]) {
+				//bonne co
+				echo "<a href=\"index.php\">revenir sur la page acceuil</a>";
+				if (isset($_SESSION["prixObjet"]) and $_SESSION["prixObjet"] < $_POST["enchere"]) {
+					$sql = "UPDATE Objet SET Uti_idUtilisateur= " . $ligne[4] . ", prixEnchere=" . $_POST["enchere"] . " WHERE nomObjet like '" . $_SESSION["nomObjet"] . "'";
+					if ($id->query($sql) === TRUE) {
+						echo "<p id=\"font\">Record updated successfully</p>";
+						if (isset($_SESSION["nbEnchere"])) {
+							$_SESSION["nbEnchere"]++;
+						} else {
+							$_SESSION["nbEnchere"] = 0;
+						}
+					} else {
+						echo "Error updating record: " . $id->error;
+					}
 				} else {
-					echo "Error updating record: " . $id->error;
+					echo "mauvais nombre";
+					if (isset($_SESSION["nomObjet"])) {
+						$file = "Location:enchere.php?nom=" . $_SESSION["nomObjet"];
+						$_SESSION["number"] = 1;
+						header($file);
+					} else {
+						header('Location:objet.php');
+					}
+					$id->close();
+					exit();
 				}
 			} else {
-				echo "mauvais nombre";
+				echo "mauvais identification";
 				if (isset($_SESSION["nomObjet"])) {
 					$file = "Location:enchere.php?nom=" . $_SESSION["nomObjet"];
-					$_SESSION["number"] = 1;
+					$_SESSION["identification"] = 1;
 					header($file);
 				} else {
 					header('Location:objet.php');
@@ -38,50 +67,44 @@ if($_POST['mail'][0]==$_POST['mail'][1]) {
 				exit();
 			}
 		} else {
-			echo "mauvais identification";
-			if (isset($_SESSION["nomObjet"])) {
-				$file = "Location:enchere.php?nom=" . $_SESSION["nomObjet"];
-				$_SESSION["identification"] = 1;
-				header($file);
+			//nouveau gars
+			$requete = "INSERT INTO utilisateur (nom, prenom, addresse, telephone)
+							VALUES ('" . $_POST['lastname'] . "', '" . $_POST['nickname'] . "', '" . $_POST['mail'][0] . "', '" . $_POST['phone'] . "')";
+			if ($id->query($requete) === TRUE) {
+				echo "<a href=\"index.php\">revenir sur la page acceuil</a>";
+				echo "<br>bonne inscription";
+				$requete = "SELECT idUtilisateur FROM utilisateur WHERE addresse like '" . $_POST['mail'][0] . "'";
+				$marche = $id->query($requete);
+				$ligne = $marche->fetch_array(MYSQLI_NUM);
+				$idUt = $ligne[0];
+				$sql = "UPDATE Objet SET Uti_idUtilisateur= " . $idUt . ", prixEnchere=" . $_POST["enchere"] . " WHERE nomObjet like '" . $_SESSION["nomObjet"] . "'";
+				if ($id->query($sql) === TRUE) {
+					echo "<br><p id=\"font\">Record updated successfully</p>";
+					if (isset($_SESSION["nbEnchere"])) {
+						$_SESSION["nbEnchere"]++;
+					} else {
+						$_SESSION["nbEnchere"] = 0;
+					}
+				} else {
+					echo "<br>Error updating record: " . $id->error;
+				}
 			} else {
-				header('Location:objet.php');
+				echo "Error insert: " . $requete . "<br>" . $id->error;
 			}
-			$id->close();
-			exit();
-		}
-	} else {
-		//nouveau gars
-		$requete = "INSERT INTO utilisateur (nom, prenom, addresse, telephone)
-							VALUES ('".$_POST['lastname']."', '".$_POST['nickname']."', '".$_POST['mail'][0]."', '".$_POST['phone']."')";
-		if ($id->query($requete) === TRUE) {
-			echo "<a href=\"index.php\">revenir sur la page acceuil</a>";
-			echo "<br>bonne inscription";
-			$requete = "SELECT idUtilisateur FROM utilisateur WHERE addresse like '" . $_POST['mail'][0] . "'";
-			$marche = $id->query($requete);
-			$ligne = $marche->fetch_array(MYSQLI_NUM);
-			$idUt=$ligne[0];
-			$sql = "UPDATE Objet SET Uti_idUtilisateur= ".$idUt.", prixEnchere=".$_POST["enchere"]." WHERE nomObjet like '".$_SESSION["nomObjet"]."'";
-			if ($id->query($sql) === TRUE) {
-				echo "<br><p id=\"font\">Record updated successfully</p>";
-			} else {
-				echo "<br>Error updating record: " . $id->error;
-			}
-		} else {
-			echo "Error insert: " . $requete . "<br>" . $id->error;
-		}
 
-	}
-}else{
-	echo "e-mail different";
-	if (isset($_SESSION["nomObjet"])) {
-		$file = "Location:enchere.php?nom=" . $_SESSION["nomObjet"];
-		$_SESSION["maildiff"] = 1;
-		header($file);
+		}
 	} else {
-		header('Location:objet.php');
+		echo "e-mail different";
+		if (isset($_SESSION["nomObjet"])) {
+			$file = "Location:enchere.php?nom=" . $_SESSION["nomObjet"];
+			$_SESSION["maildiff"] = 1;
+			header($file);
+		} else {
+			header('Location:objet.php');
+		}
+		$id->close();
+		exit();
 	}
-	$id->close();
-	exit();
 }
 
 $id->close();
